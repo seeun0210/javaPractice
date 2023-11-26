cb9026e71a624abd053bb56c9f838b6a447ca79e
KimSeEun <cocobell3@naver.com>
2023-11-26 21:30:00 +0900

    study: java 중첩반복문, break, continue

---
+++ src/ch17/BreakTest.java
@@ -0,0 +1,16 @@
+package ch17;
+
+public class BreakTest {
+    public static void main(String[] args) {
+        int sum=0;
+        int num;
+        for(num=1; true; num++){
+            sum+=num;
+            if(sum>100)
+                break;
+
+        }
+        System.out.println(sum);
+        System.out.println(num);//14까지 더했을 때 105가 되었고, 15가 되었을 때 빠져나옴
+    }
+}
