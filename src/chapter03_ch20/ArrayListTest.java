db417af3b653b7667a219e28615dacf44ae2ac37
kimseeun <cocobell3@naver.com>
2023-11-30 17:33:42 +0900

    study: 객체 배열을 구현한 ArrayList

---
+++ src/chapter03_ch20/ArrayListTest.java
@@ -0,0 +1,19 @@
+package chapter03_ch20;
+
+import chapter03_ch18.Book;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+        ArrayList<Book> list= new ArrayList();
+        list.add(new Book("태백산맥1","조정래"));
+        list.add(new Book("태백산맥2","조정래"));
+        list.add(new Book("태백산맥3","조정래"));
+        list.add(new Book("태백산맥4","조정래"));
+        list.add(new Book("태백산맥5","조정래"));
+        for(int i=0;i<list.size();i++){
+            list.get(i).showBookInfo();
+        }
+    }
+}
